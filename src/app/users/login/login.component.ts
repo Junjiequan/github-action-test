530c508544c5e33550beeda8f6a33166b71e5bd3
junjiequan <a331998513@gmail.com>
2024-04-10 10:41:00 +0200

    hey!

--- src/app/users/login/login.component.ts
+++ src/app/users/login/login.component.ts
@@ -77,10 +77,8 @@ export class LoginComponent implements OnInit, OnDestroy {
 
   redirectOIDC(authURL: string) {
     this.document.location.href = `${this.appConfig.lbBaseURL}/${authURL}`;
-    console.log("-hey1");
-    console.log("-hey2");
-    console.log("-hey3");
-    console.log("-hey4");
+
+    console.log("-Breaking change");
   }
 
   openPrivacyDialog() {
