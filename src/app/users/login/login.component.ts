fc82e2d2c698da700e22199b58b68b9addf6cfed
Jay Quan <a331998513@gmail.com>
2024-04-15 13:05:44 +0200

    fix: test fix hello-1 (#33)

--- src/app/users/login/login.component.ts
+++ src/app/users/login/login.component.ts
@@ -79,9 +79,11 @@ export class LoginComponent implements OnInit, OnDestroy {
     this.document.location.href = `${this.appConfig.lbBaseURL}/${authURL}`;
 
 
+
     console.log("-Breaking change123");
     console.log("-Breaking 1");
     console.log("-Breaking 2");
+
 
   }
 
